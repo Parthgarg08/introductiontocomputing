7b87e3872dd3b7e6ced0d1e80152ae5cd42b92bd
Parthgarg08 <104362070+Parthgarg08@users.noreply.github.com>
2022-05-14 23:20:25 +0530

    Create Assignment2_output.py

---
+++ Assignment2_output.py
@@ -0,0 +1,30 @@
+Answer 1
+a. 35
+b. egaugnal evitisnes esac a si nohtyP
+c. a case sensitive
+d. Python is object oriented language
+e. 10
+f. Pythonisacasesensitivelanguage
+Answer 2
+Hey, Yuvraj Singh Birdi here!
+My SID is 21103049
+I am from CSE department and my CGPA is 9.90
+Answer 3
+a. 8
+b. 58
+c. 50
+d. a would become 224 and b would become 40
+e. a would become 14 and b would become 0
+Answer 4
+Enter first number: 24
+Enter second number: 52
+Enter third number: 11
+52  is the largest number.
+Answer 5
+Enter a word to check: tournament   
+Yes
+Answer 6
+Enter length of first side: 3
+Enter length of second side: 4
+Enter length of third side: 5
+Yes

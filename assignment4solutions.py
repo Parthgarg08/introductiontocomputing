ca5497e878a77b937dd6b63566b898f51977b176
Parthgarg08 <104362070+Parthgarg08@users.noreply.github.com>
2022-06-19 21:06:22 +0530

    Update assignment4solutions.py

--- assignment4solutions.py
+++ assignment4solutions.py
@@ -1,7 +1,4 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 
-=============== RESTART: C:/Users/lenovo/Desktop/assignment 4.py ===============
 Q1
 Enter mark:50
 Grade: C

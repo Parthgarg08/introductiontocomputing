32cbee6370a03a4932dd4ce50a6f495e12f39d5c
Parthgarg08 <104362070+Parthgarg08@users.noreply.github.com>
2022-07-11 09:17:51 +0530

    Create assignment 6_output.py

---
+++ assignment 6_output.py
@@ -0,0 +1,90 @@
+#Output for question 1
+
+Enter a number: 56
+56  is not a perfect number
+
+Enter a number: 28
+28  is a perfect number
+
+
+#Output for question 2
+
+Enter a String: madam
+madam is a palindrome
+
+Enter a String: aryan
+aryan is not a palindrome
+
+
+#Output for question 3
+
+Enter a number: 7
+        1 
+       1 1 
+      1 2 1 
+     1 3 3 1 
+    1 4 6 4 1 
+   1 5 10 10 5 1 
+  1 6 15 20 15 6 1 
+      
+
+#Output for question 4
+
+Enter a String: I love my country
+No 'I love my country' is not a panagram
+
+Enter a String:  Pack my box with five dozen liquor jugs
+Yes ' Pack my box with five dozen liquor jugs' is a panagram
+
+
+#Output for question 5
+
+Enter a hyphen separated sentence: I-love-to-play-badminton
+I-badminton-love-play-to
+
+
+#Output for question 6
+
+Student ID= 21102097
+Student ID= 21102097
+Student Name= Aryan Kumar
+Student ID= 21102097
+Student Name= Aryan Kumar
+Student Class= 1st Year
+
+
+#Output for question 7
+
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+
+
+#Output for question 8
+
+Enter number of elements : 8
+-25
+-10
+-7
+-3
+2
+4
+8
+10
+-10 2 8
+-7 -3 10
+None
+
+
+#Output for question 9
+
+Enter the sequence of parantheses : {}[]()
+{}[]() - is balanced
+
+Enter the sequence of parantheses : ()[]{}
+()[]{} - is balanced
